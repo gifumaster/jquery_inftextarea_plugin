8f32bb6eface309ef5cbff8b9c2aa80f696910c5
gifumaster <gifumaster@gmail.com>
2022-10-03 07:39:36 +0900

    一旦コミット

--- src/inftextarea.js
+++ src/inftextarea.js
@@ -8,12 +8,14 @@
             // ボタンの表示名
             buttonPlusString: '+',
             buttonMinusString: '-',
-            // テキストの合体対象のクラス。（基本いじらなくて良い）
+            // 各要素のCSS用クラス
+            cssClassForTextarea: '',
+            cssClassForButtonMinus: '',
+            cssClassForButtonPlus: '',
+            // テキストのカウント対象のクラス。（基本いじらなくて良い）複数種類設置する場合に指定する。
             textareaClass: '_inf_target_textarea',
-            // テキスト合体時に間に入る文字列
-            concatDelimiter: "\n---\n",
             // 要素に結果を出力する場合に指定。
-            outputElementID: '',
+            outputTextElementID: ''
         }
         let setting = $.extend(defaults, options);
 
@@ -21,61 +23,105 @@
             return getResult();
         }
 
+        let groupCount = 0;
         return this.each(function () {
+            const localCount = groupCount;
             let $this = $(this);
 
-            // ボタンを生成
-            const plusButton = $('<button>').text(setting.buttonPlusString).addClass(['_inf_button_plus', '_inf_button']).click(() => {
-                if (countTextarea() < setting.maxTextAreaLength) {
-                    $this.append(generateTextarea());
-                }
-            });
-
-            $this.append(plusButton);
-
             for (let i = 0; i < setting.initialTextAreaLength; i++) {
-                $this.append(generateTextarea());
+                $this.append(generateTextarea(localCount));
             }
+
+            groupCount++;
         });
 
         function countTextarea() {
             return $('.' + setting.textareaClass).length;
         }
 
-        function getResult() {
-            let concatText = '';
-
-            $('.' + setting.textareaClass).each(function () {
-                let $this = $(this);
-                concatText += $this.val();
-                concatText += setting.concatDelimiter;
-            });
-
-            return concatText;
+        function countGroupTextarea(groupCount) {
+            return $('.' + setting.textareaClass + '_group' + groupCount).length;
         }
 
-        function generateTextarea() {
+        function getResult() {
+            let currentTitle = null;
+            let concatText = '';
+            let count = 0;
 
-            const textarea = $('<textarea>').addClass('_inf_textarea');
+            $('.' + setting.textareaClass).each(function () {
+                const $this = $(this);
+                const title = $this.parent().parent().data('name');
+
+                if (title !== currentTitle) {
+                    concatText += '【' + title + '】\n';
+                }
+
+                if($this.val() !== '')
+                {
+                    concatText += '・' + $this.val() + '\n';
+                    count++;
+                }
+
+                currentTitle = title;
+            });
+
+            return [concatText,count];
+        }
+
+        function generateTextarea(groupCount) {
+
+            const textarea = $('<input>').addClass('_inf_textarea');
             if (setting.textareaClass !== '') {
-                textarea.addClass(setting.textareaClass);
+                textarea.addClass(setting.textareaClass).addClass(setting.textareaClass + '_group' + groupCount);
             }
+            if (setting.cssClassForTextarea !== '')
+            {
+                textarea.addClass(setting.cssClassForTextarea);
+            }
+
             if (setting.outputElement !== '') {
                 textarea.change(function () {
-                    $('#' + setting.outputElement).val(getResult());
+                    [text,count] = getResult();
+                    $('#' + setting.outputTextElementID).val(text);
+                    $('#' + setting.outputCountElementID).val(count);
                 });
             }
-
 
             const div = $('<div>').append(textarea).addClass('_inf_div');
 
             const minusButton = $('<button>').text(setting.buttonMinusString).addClass(['_inf_button_minus', '_inf_button']).click(() => {
-                //自分の中のdivが最後2つ以上あれば自身を削除する。
-                if (countTextarea() > 1) {
+                div.find('._inf_textarea').val('');
+                if (countGroupTextarea(groupCount) > 1) {
                     div.remove();
                 }
+                if (countTextarea() < setting.maxTextAreaLength) {
+                    $('._inf_button_plus').prop('disabled', false).attr('title', '');
+                }
             });
+            if (setting.cssClassForButtonMinus !== '')
+            {
+                minusButton.addClass(setting.cssClassForButtonMinus);
+            }
+
+
             div.append(minusButton);
+
+            const plusButton = $('<button>').text(setting.buttonPlusString).addClass(['_inf_button_plus', '_inf_button']).click(() => {
+                if (countTextarea() < setting.maxTextAreaLength) {
+                    div.after(generateTextarea(groupCount));
+                }
+
+                if (countTextarea() === setting.maxTextAreaLength) {
+                    $('._inf_button_plus').prop('disabled', true).attr('title', 'これ以上増やせません');
+                }
+            });
+            if (setting.cssClassForButtonPlus !== '')
+            {
+                minusButton.addClass(setting.cssClassForButtonPlus);
+            }
+
+            div.append(plusButton);
+
             return div;
         }
     };
